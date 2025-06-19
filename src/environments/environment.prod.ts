5eaa49804c3d1c76166e424f83545bde76aecb02
Lynx Pardelle <lynxpardelle@lynxpardelle.com>
2025-06-18 23:46:13 -0600

    Update API URLs in environment configurations for production and development

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.sokoke.com'
+  apiUrl: 'https://api-bridge.lynxpardelle.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4003'
+  apiUrl: 'http://localhost:1666'
 };
